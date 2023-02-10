9f559a8e5a795c60bd1129e6235281d75e287bbb
Anand.cloud_user <anand@email>
2023-02-10 09:06:02 +0530

    first commit

---
+++ src/Components/ButtonApp.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function ButtonApp(){
+    return (
+        <div>
+            <Button variant='contained'>Hello Worold</Button>
+        </div>
+    );
+}
+
+export default ButtonApp;
---
+++ src/Components/Table.js
@@ -0,0 +1,8 @@
+function XTable(props){
+    return <table>
+            <tr><td>Table ID = {props.tableId}</td><td>Table ID = {props.tableId}</td></tr>
+            <tr><td>Table ID = {props.tableId}</td><td>Table ID = {props.tableId}</td></tr>            
+        </table>;
+}
+
+export default XTable;
